0cf1e61826f7113d7abfe8154524bfc0e56a8ec0
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2024-12-05 10:23:34 +0300

    Added some adjustment using javascript to help modify the page outlook

--- Js/index.js
+++ Js/index.js
@@ -1,2 +1,17 @@
-alert("Welcome to Branice PortFolio");
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = "";
+    letters.forEach((letter) => {
+        let  span = document.createElement("span");
+        span.textContent = letters;
+        span.className = "letters";
+        word.append(span);
+    })
+});
 
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let change
